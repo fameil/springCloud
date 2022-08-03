7cb87bdf8c94fa0daf55abd9d0eca763a41aeaaa
fameil <847144514@qq.com>
2022-08-03 10:09:28 +0800

    first commit2

--- cloud-gateway-gateway9527/src/main/java/com/srz/springcloud/GateWayMain9527.java
+++ cloud-gateway-gateway9527/src/main/java/com/srz/springcloud/GateWayMain9527.java
@@ -13,5 +13,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class GateWayMain9527 {
     public static void main(String[] args) {
         SpringApplication.run(GateWayMain9527.class,args);
+        System.out.println("hello git");
     }
 }
